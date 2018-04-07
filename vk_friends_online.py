56f0b32eed78468a4f80b1ac4c0870a518cc1625
Stanislav <37021640+oeananas@users.noreply.github.com>
2018-04-07 23:40:47 +0300

    Update vk_friends_online.py
    рабочий код

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,15 +1,17 @@
 import vk
+import getpass
 
-
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = 6440852
 
 
 def get_user_login():
-    pass
+    login = input('VK Login: ')
+    return login
 
 
 def get_user_password():
-    pass
+    password = getpass.getpass(prompt='VK Password: ')
+    return password
 
 
 def get_online_friends(login, password):
@@ -17,16 +19,25 @@ def get_online_friends(login, password):
         app_id=APP_ID,
         user_login=login,
         user_password=password,
+        scope='friends'
     )
     api = vk.API(session)
-    # например, api.friends.get()
+    friends_online_ids = api.friends.getOnline(v=5.52)
+    friends_online = api.users.get(v=5.52, user_ids=friends_online_ids)
+    return friends_online
 
 
 def output_friends_to_console(friends_online):
-    pass
+    print('Your VK friends online:')
+    for friend in friends_online:
+        print(friend['first_name'], friend['last_name'])
+
 
 if __name__ == '__main__':
-    login = get_user_login()
-    password = get_user_password()
-    friends_online = get_online_friends(login, password)
-    output_friends_to_console(friends_online)
+    try:
+        login = get_user_login()
+        password = get_user_password()
+        friends_online = get_online_friends(login, password)
+        output_friends_to_console(friends_online)
+    except vk.exceptions.VkAuthError:
+        exit('You entered incorrect login / password')
